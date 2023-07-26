a3c8833dc231cca6249bdeb5ebf9c558fff7c07b
Beket <dattebayokz@gmail.com>
2023-07-26 22:08:16 +0600

    edit auth

--- app/auth/router/router_authorize_user.py
+++ app/auth/router/router_authorize_user.py
@@ -9,6 +9,11 @@ from . import router
 from .errors import InvalidCredentialsException
 
 
+class AuthorizeUserRequest(AppModel):
+    email: str
+    password: str
+
+
 class AuthorizeUserResponse(AppModel):
     access_token: str
     token_type: str = "Bearer"
@@ -16,15 +21,16 @@ class AuthorizeUserResponse(AppModel):
 
 @router.post("/users/tokens", response_model=AuthorizeUserResponse)
 def authorize_user(
-    input: OAuth2PasswordRequestForm = Depends(),
+    # input: OAuth2PasswordRequestForm = Depends(),
+    input: AuthorizeUserRequest = Depends(),
     svc: Service = Depends(get_service),
 ) -> AuthorizeUserResponse:
-    user = svc.repository.get_user_by_email(input.username)
-
+    # user = svc.repository.get_user_by_email(input.username)
+    user = svc.repository.get_user_by_email(input.dict()["email"])
     if not user:
         raise InvalidCredentialsException
 
-    if not check_password(input.password, user["password"]):
+    if not check_password(input.dict()["password"], user["password"]):
         raise InvalidCredentialsException
 
     return AuthorizeUserResponse(
