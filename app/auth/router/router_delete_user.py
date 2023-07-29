dd290ec37cd59bcb519b6a4df4d9a714e8b700c1
Beket <dattebayokz@gmail.com>
2023-07-29 18:31:41 +0600

    fixed user deletion

--- app/auth/router/router_delete_user.py
+++ app/auth/router/router_delete_user.py
@@ -1,6 +1,8 @@
 from fastapi import Depends, Response
 from ..adapters.jwt_service import JWTData
 from ..service import Service, get_service
+from app.trips.service import Service as TripsService
+from app.trips.service import get_service as get_trips_service
 from . import router
 from .dependencies import parse_jwt_user_data
 
@@ -9,6 +11,10 @@ from .dependencies import parse_jwt_user_data
 def delete_user(
     jwt_data: JWTData = Depends(parse_jwt_user_data),
     svc: Service = Depends(get_service),
+    trips_svc: TripsService = Depends(get_trips_service)
 ):
     svc.repository.delete_user(user_id=jwt_data.user_id)
+    trips = svc.repository.get_trips(user_id=jwt_data.user_id)
+    for trip in trips:
+        trips_svc.repository.delete_trip(user_id=jwt_data.user_id, trip_id=trip["_id"])
     return Response(status_code=200)
