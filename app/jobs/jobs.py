544f1ef72a583ec4e50a4ed4d8daa241b17ffc19
Beket <dattebayokz@gmail.com>
2023-08-01 21:05:07 +0600

    changed scheduler from cron to 1 hour interval

--- app/jobs/jobs.py
+++ app/jobs/jobs.py
@@ -4,5 +4,5 @@ from .tasks import scrape_news
 
 scheduler = BackgroundScheduler()
 
-scheduler.add_job(scrape_news, "cron", hour=19, minute=54)
-# scheduler.add_job(scrape_news, "interval", hours=1)
+# scheduler.add_job(scrape_news, "cron", hour=19, minute=54)
+scheduler.add_job(scrape_news, "interval", hours=1)
