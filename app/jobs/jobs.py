55e2e9c2d35dfc31c665d284924912baede0f2a6
Beket <dattebayokz@gmail.com>
2023-08-01 14:57:31 +0600

    added news routers

--- app/jobs/jobs.py
+++ app/jobs/jobs.py
@@ -4,6 +4,6 @@ from tasks import scrape_news
 
 scheduler = BackgroundScheduler()
 
-scheduler.add_job(scrape_news, trigger="cron", hour=14, minute=30)
+scheduler.add_job(scrape_news, trigger="cron", hour=15, minute=5)
 
 
